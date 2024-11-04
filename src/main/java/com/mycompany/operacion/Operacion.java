5cfcfd9868bad01e507ae5227ab25381853c25fc
lourinho <72151685+lourinho18@users.noreply.github.com>
2024-11-04 17:57:00 +0100

    Operacion.java

--- src/main/java/com/mycompany/operacion/Operacion.java
+++ src/main/java/com/mycompany/operacion/Operacion.java
@@ -8,22 +8,4 @@ package com.mycompany.operacion;
  *
  * @author lourinho
  */
-import java.util.Scanner;
-
-public class Operacion {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Introduce el primer número: ");
-        double numero1 = scanner.nextDouble();
-
-        System.out.print("Introduce el segundo número: ");
-        double numero2 = scanner.nextDouble();
-
-        double resultado = numero1 - numero2;
-        System.out.println("El resultado de la resta es: " + resultado);
-
-        scanner.close();
-    }
-}
 
