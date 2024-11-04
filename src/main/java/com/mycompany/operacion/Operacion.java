5b891191090e9d6c2e6132dad7de825e2bff0187
niky29om <andrea.ortiz.mendez@colexio-karbo.com>
2024-11-04 18:03:14 +0100

    Añadida la longitud del circulo

--- src/main/java/com/mycompany/operacion/Operacion.java
+++ src/main/java/com/mycompany/operacion/Operacion.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.operacion;
+
+import java.util.Scanner;
 
 /**
  *
